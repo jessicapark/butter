b731bf24463c28c35a1ec86d933894d940335000
JessicaPark <jessicapark@jessicaparkdeMacBook-Pro.local>
2014-09-30 21:56:51 +0800

    b

--- javascripts/main.js
+++ javascripts/main.js
@@ -30,13 +30,11 @@ function adjustWindow(){
 	if (wrapperInnerW/wrapperInnerH<2.054) {
 		$('.sec1-bg').height("100%");
 		$('.sec1-bg').width("auto");
-		var bgW = $('.sec1-bg').width();
-		$('.sec1-bg').css({left: '50%'});
-		$('.sec1-bg').css({marginLeft: - bgW/2 +'px'});
 	}else{
 		$('.sec1-bg').height("auto");
 		$('.sec1-bg').width("100%");
-		$('.sec1-bg').css({left: '0'});
-		// $('.sec1-bg').css({marginLeft: '0'});
 	};
+	var bgW = $('.sec1-bg').width();
+	$('.sec1-bg').css({left: '50%'});
+	$('.sec1-bg').css({marginLeft: - bgW/2 +'px'});
 }
